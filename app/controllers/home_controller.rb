fd8c36ebf79abaf77c17df4d8cb5458909b272b7
古宮信二 <>
2025-11-03 09:32:14 +0900

    device を使った表示変更最終処理

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 class HomeController < ApplicationController
+  helper_method :resource, :resource_name, :devise_mapping
+
   def index
-    render 'home/index'
+    # Deviseの変数をセット
+    @resource = User.new
+    @resource_name = :user
+    @devise_mapping = Devise.mappings[:user]
+  end
+
+  private
+
+  def resource
+    @resource
+  end
+
+  def resource_name
+    @resource_name
+  end
+
+  def devise_mapping
+    @devise_mapping
   end
 end
