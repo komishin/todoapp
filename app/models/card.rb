2bdfbd14aee3bd262e35a6a927b4f2c1607a9caa
古宮信二 <>
2025-11-15 13:06:35 +0900

    task一覧を表示できるようにした

---
+++ app/models/card.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: cards
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  deadline    :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Card < ApplicationRecord
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_04_082248) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_15_035821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_04_082248) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "deadline"
+    t.text "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
